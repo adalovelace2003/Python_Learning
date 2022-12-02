ba07b76f61ee0079acd7c6dd3839506f1a3a8425
adalovelace2003 <adalovelace2003@github.com>
2022-12-02 11:28:04 -0500

    Day 2

---
+++ DAY2/02_CWH_comments_and_escape_sequence_character.py
@@ -0,0 +1,26 @@
+print("\n\n\t\tOutput : \n\n\n\n")
+# This is comment , you can use \n for break characters.
+# Author : Ada Lovelace
+
+# print(("\n\nRam is boy \n\nSita is girl"))
+
+
+''' This is multi line comment 
+print("Hello World") #This prints hello world
+'''
+
+# Escape sequnce characters
+
+# \n  to add new line 
+# \t tp add 
+print("\n Hello,  \n\tAsk Buddie Members")
+
+# \" to add inverted comma
+print("\n\n             \" i am inside inverted comma\"     ")
+
+# prnting multiple values    # USE SEP TO ADD SEPARATORS
+print("Ada", 6, 7, sep="~", end="009\n")
+
+
+
+
