6912ff70b9eb696f9fd7cfbc7ba5ced10c48d206
adalovelace2003 <adalovelace2003@github.com>
2022-12-04 11:51:32 -0500

    Day 4

---
+++ DAY4/16_calculator.py
@@ -0,0 +1,31 @@
+print("\n\n\n\n\n\n")
+
+num_1 = int(input('Enter your first number: '))
+num_2 = int(input('Enter your second number: '))
+
+# Addition
+# print('\n\n {} + {} = '.format(num_1, num_2))
+# print(num_1 + num_2)
+
+print((num_1) + "+" + (num_2) + "=" + (num_1+num_2) )
+
+# print("\n")
+# # Subtraction
+# print('{} - {} = '.format(num_1, num_2))
+# print(num_1 - num_2)
+
+# print("\n")
+# # Multiplication
+# print('{} * {} = '.format(num_1, num_2))
+# print(num_1 * num_2)
+
+# print("\n")
+# # Division
+# print('{} / {} = '.format(num_1, num_2))
+# print(num_1 / num_2)
+
+print("\n\n\n\n\n\n")
+
+
+
+
