796af0e9368ff9786efdff67c14ee4538c385818
adalovelace2003 <adalovelace2003@github.com>
2022-12-08 12:22:10 -0500

    Day 8

---
+++ DAY8/35_input.py
@@ -0,0 +1,15 @@
+a = input("Enter your name : ")
+print(" My name is ",a)
+
+
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+
+print("Sum is ",a+b)
+
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+
+print("Sum is ",a+b)
---
+++ DAY8/37_string_method.py
@@ -0,0 +1,10 @@
+name = "Guzman"
+age = 18
+statement = "My name is {} and my age is {}."
+print(statement.format(name, age)) 
+
+print("My name is "+ name+ " and age is "+ str(age))
+
+
+str3 = "What will you eat? Apple\\Banana"
+print(str3)
