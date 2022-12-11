4b7751a8448757e4be2ad86ddb235c43985d92d7
adalovelace2003 <adalovelace2003@github.com>
2022-12-11 21:07:51 +0545

    Day 11

---
+++ DAY11/46_function_arguments.py
@@ -0,0 +1,28 @@
+print("\n\n\n\n\n\n")
+# Function Arguments
+''' 
+Default Arguments
+Keyword Arguments
+Required Arguments
+Variable-length Arguments
+'''
+
+# Default Arguments
+def name(fname, mname = "Jhon", lname = "Whatson"):
+    print("Hello,", fname, mname, lname)
+
+name("Amy")
+
+# Keyword Arguments
+def name(fname, mname, lname):
+    print("Hello,", fname, mname, lname)
+
+name(mname = "Peter", lname = "Wesker", fname = "Jade")
+
+# Required Arguments
+def name(fname, mname, lname):
+    print("Hello,", fname, mname, lname)
+
+# name("Peter", "Quill")    ''' This will give error '''
+
+print("\n\n\n\n\n\n")
