1a2d791f9bb5ff15a54a4838cb3bf0fb2e69afc0
adalovelace2003 <adalovelace2003@github.com>
2022-12-14 09:47:55 +0545

    Day 13

--- DAY13/48_say_hi_hello_in_ordero_of_time.py
+++ DAY13/48_say_hi_hello_in_ordero_of_time.py
@@ -2,10 +2,10 @@ import time
 timestamp = time.strftime('%H:%M:%S')
 print(timestamp)
 h = int(time.strftime('%H'))
-print('\n',h)
+print('\n', h)
 if h >= 6 and h < 12:
     print("\n\nGood Morning !")
-elif h >=  12 and h < 17:
+elif h >= 12 and h < 17:
     print("\n\nGood Afternoon !")
 elif h >= 17 and h < 20:
     print("\n\nGood Evening !")
