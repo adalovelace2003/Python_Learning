5ab258ee3bc063531bf77f0ab1efd9c3d835fd34
adalovelace2003 <adalovelace2003@github.com>
2022-12-14 09:48:37 +0545

    Day 13

--- DAY13/49_match_case_statements.py
+++ DAY13/49_match_case_statements.py
@@ -1,9 +1,9 @@
-x = int(input("Enter the value of x"))
-match x:
-    case 0:
-        print(' x is zero')
-    case 4:
-        print(' case is 4')
-    case _:
-        print(x)
+# x = int(input("Enter the value of x"))
+# match x:
+#     case 0:
+#         print(' x is zero')
+#     case 4:
+#         print(' case is 4')
+#     case _:
+#         print(x)
         
