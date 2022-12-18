29601242df6153bc9d4579ed5861e3b311a8795b
adalovelace2003 <adalovelace2003@github.com>
2022-12-18 20:00:40 +0545

    Day 16

---
+++ DAY16/57_using_different_python_modules.py
@@ -0,0 +1,7 @@
+import math
+
+print("Sin(0) =", math.sin(0))
+print("Sin(30) =", math.sin(math.pi/6))
+print("Sin(45) =", math.sin(math.pi/4))
+print("Sin(60) =", math.sin(math.pi/3))
+print("Sin(90) =", math.sin(math.pi/2))
---
+++ DAY16/58_using_module.py
@@ -0,0 +1,6 @@
+import "DAY16/module.py"
+
+a = 1
+b = 2
+
+print(add(a,b))
