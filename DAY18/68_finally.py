d585034283a674d710f8240e34b5f794d03ec5ed
adalovelace2003 <adalovelace2003@github.com>
2022-12-18 20:38:49 +0545

    Day 18

---
+++ DAY18/68_finally.py
@@ -0,0 +1,7 @@
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+  
---
+++ DAY18/70_raise.py
@@ -0,0 +1,5 @@
+x = "hello"
+
+if not type(x) is int:
+  raise TypeError("Only integers are allowed")
+
