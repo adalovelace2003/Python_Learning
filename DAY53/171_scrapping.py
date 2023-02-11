a1897a639404e1d6f3c55c6fd122595b8152273e
Ada Lovelace <117154139+adalovelace2003@users.noreply.github.com>
2023-02-11 16:08:27 +0000

    Web Scrapping

---
+++ DAY53/171_scrapping.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
+base_url = "https://www.scrapethissite.com/pages/forms/"
+soup = BeautifulSoup(requests.get(base_url).content, "html.parser")
+All = soup.find_all("tr", {"class": "team"})
+
+All_array = [1,2]
+l = []
+d = {}
+for index, item in enumerate(All_array):
+# for index, item in enumerate(All[:-1]):
+    team_name = All[index].find_all("td", {"class": "name"})
+    # print(index,team_name[0].text.strip())
+    year = All[index].find_all("td", {"class": "year"})
+    wins = All[index].find_all("td", {"class": "wins"})
+    losses = All[index].find_all("td", {"class": "losses"})
+    otLosses = All[index].find_all("td", {"class": "ot-losses"})
+    pct = All[index].find_all("td", {"class": "pct"})
+    gf = All[index].find_all("td", {"class": "gf"})
+    ga = All[index].find_all("td", {"class": "ga"})
+    diff = All[index].find_all("td", {"class": "diff"})
+
+    d["team_name"] = team_name[0].text.strip() if team_name else "N/A"
+    d["year"] = year[0].text.strip() if year else "N/A"
+    d["wins"] = wins[0].text.strip() if wins else "N/A"
+    d["losses"] = losses[0].text.strip() if losses else "N/A"
+    d["otLosses"] = otLosses[0].text.strip() if otLosses else "N/A"
+    d["pct"] = pct[0].text.strip() if pct else "N/A"
+    d["gf"] = gf[0].text.strip() if gf else "N/A"
+    d["ga"] = ga[0].text.strip() if ga else "N/A"
+    d["diff"] = diff[0].text.strip() if diff else "N/A"
+
+    print(d)
+    l.append(d)
+
+df = pandas.DataFrame(l)
+# print(l)
+# df.to_csv("DAY53/Output.csv")
+# print(All[0])
